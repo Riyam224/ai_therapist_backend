75bc134c67e4f5998b5c2d1a91bef598c96f4eb7
Riyam <cs50riyam211@gmail.com>
2026-07-01 09:57:36 +0300

    Use Postgres in production via DATABASE_URL, pin dj-database-url

--- core/settings.py
+++ core/settings.py
@@ -3,6 +3,8 @@
 # -----------------------------------------------------
 import os
 from pathlib import Path
+import dj_database_url
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,12 +77,20 @@ WSGI_APPLICATION = "core.wsgi.application"
 # -----------------------------------------------------
 # Database
 # -----------------------------------------------------
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 # -----------------------------------------------------
 # Password validation
